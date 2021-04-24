47a373758213964a0ebd901922390628d20d9bee
tuenguyen <tue.nguyen.2411@gmail.com>
2021-04-24 07:10:28 +0700

    add lenet practice

---
+++ samples/lenet/build/CMakeFiles/mnist.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/custom/samples/lenet/mnist.cpp" "/home/custom/samples/lenet/build/CMakeFiles/mnist.dir/mnist.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
